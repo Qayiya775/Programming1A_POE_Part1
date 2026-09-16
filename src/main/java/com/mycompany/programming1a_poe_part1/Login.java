a88d1c5937f50d0d409296a2e09ce524b4fedffb
Qayiya775 <ST10510290@rcconnect.edu.za>
2026-09-16 17:13:39 +0200

    Added password complexity validation

--- src/main/java/com/mycompany/programming1a_poe_part1/Login.java
+++ src/main/java/com/mycompany/programming1a_poe_part1/Login.java
@@ -11,13 +11,40 @@ package com.mycompany.programming1a_poe_part1;
 public class Login {
     
     private String username;
+    private String password;
     
-    public Login(String username){
+    public Login(String username,String password){
         this.username=username;
+        this.password=password;
     }
     
     public boolean checkUserName() {
         return username.contains("_")&& username.length()<=5; 
     }
+public boolean checkPasswordComplexity() {
+    boolean hasCapital= false;
+    boolean hasNumber= false;
+    boolean hasSpecialCharacter=false;
+     
+    if (password.length()<8) {
+        return false;
+    }
+    for (int i = 0; i < password.length(); i++) {
+        char currentCharacter= password.charAt(i);
+        
+        if (Character.isUpperCase(currentCharacter)){
+            hasCapital= true;
+        }
+        
+        if (Character.isDigit(currentCharacter)) {
+            hasNumber = true;
+        }
+        
+         if (!Character.isLetterOrDigit(currentCharacter)) {
+        hasSpecialCharacter = true;
+         }
+    }
     
+    return hasCapital && hasNumber && hasSpecialCharacter;
+}
 }
